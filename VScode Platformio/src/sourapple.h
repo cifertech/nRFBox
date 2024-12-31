88827478f61a9e64825a1875e97313c4bb29fdaa
zRCrackiiN <124148929+zRCrackiiN@users.noreply.github.com>
2024-12-31 21:50:14 +1100

    Platform.io Integration
    integrated platform.io integration

---
+++ VScode Platformio/src/sourapple.h
@@ -0,0 +1,17 @@
+/* ____________________________
+   This software is licensed under the MIT License:
+   https://github.com/cifertech/nrfbox
+   ________________________________________ */
+   
+#ifndef sourapple_H
+#define sourapple_H
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <U8g2lib.h>
+
+void sourappleSetup();
+void sourappleLoop();
+
+#endif
